06e234cd50c918a87a7827712abeecf0a81733af
Ruslan Ismagilov <is.ismagilov@gmail.com>
2014-12-29 23:27:13 +0300

    test: update

--- test.js
+++ test.js
@@ -7,12 +7,6 @@ var fs = require('fs');
 var files = glob.sync('exercises/*/solution/solution.js');
 
 files.forEach(function (file) {
-  var name = path.basename(path.dirname(path.dirname(file)));
-  execSync('./demo-workshopper.js select ' + name);
-  var submission = execSync('./demo-workshopper.js run ' + file).toString();
-  var result = fs.readFileSync(file.replace(/\.js$/, '.txt'), 'utf8');
-  assert(submission === result);
-
   // check verify output include solution file
   var v = execSync('./demo-workshopper.js verify ' + file).toString();
   var solution = fs.readFileSync(file, 'utf8');
