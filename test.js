200dbe22f00b74a4199a66b675fdf1fd5501ec37
Ruslan Ismagilov <is.ismagilov@gmail.com>
2014-12-29 23:06:59 +0300

    test: add

---
+++ test.js
@@ -0,0 +1,20 @@
+var execSync = require('child_process').execSync;
+var assert = require('assert');
+var glob = require('glob');
+var path = require('path');
+var fs = require('fs');
+
+var files = glob.sync('exercises/*/solution/solution.js');
+
+files.forEach(function (file) {
+  var name = path.basename(path.dirname(path.dirname(file)));
+  execSync('./demo-workshopper.js select ' + name);
+  var submission = execSync('./demo-workshopper.js run ' + file).toString();
+  var result = fs.readFileSync(file.replace(/\.js$/, '.txt'), 'utf8');
+  assert(submission === result);
+
+  // check verify output include solution file
+  var v = execSync('./demo-workshopper.js verify ' + file).toString();
+  var solution = fs.readFileSync(file, 'utf8');
+  assert(v.replace(/\s*/g, '').indexOf(solution.replace(/\s*/g, '')) >= 0);
+});
