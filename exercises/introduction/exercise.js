8375e121b41c1becee43f84961d948e2324b610d
Ruslan Ismagilov <is.ismagilov@gmail.com>
2015-01-01 13:13:08 +0300

    exercise: checkify

--- exercises/introduction/exercise.js
+++ exercises/introduction/exercise.js
@@ -33,24 +33,6 @@ exercise.addSetup(function (mode, callback) {
     process.nextTick(callback);
 });
 
-// add a processor for both run and verify calls, added *before*
-// the comparestdout processor so we can mess with the stdouts
-exercise.addProcessor(function (mode, callback) {
-
-    this.submissionStdout.pipe(process.stdout);
-
-    // replace stdout with our own streams
-    this.submissionStdout = through2();
-    if (mode == 'verify') {
-        this.solutionStdout = through2();
-    }
-
-    process.nextTick(function () {
-        callback(null, true)
-    });
-});
-
-// compare stdout of solution and submission
 exercise = comparestdout(exercise)
 
 module.exports = exercise;
